12dff24047aa315454e7187814e9ccb0b26f5632
tsoteen89 <fapppo@Traviss-Air.att.net>
2015-02-14 20:54:54 -0600

    Added Graphics to BullsEye

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Travis Osteen on 2/14/15.
+//  Copyright (c) 2015 Travis Osteen. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
